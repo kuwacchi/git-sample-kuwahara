75ce260e3dc26fadd31b56b1b1bd64aa416963a0
kuwaharakengo <kengo.kuwahara@rakus.co.jp>
2017-06-14 10:29:42 +0900

    gitテスト用の新規プロジェクト作成

---
+++ src/main/java/com/sample/demo/GitSampleKuwaharaApplication.java
@@ -0,0 +1,12 @@
+package com.sample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKuwaharaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKuwaharaApplication.class, args);
+	}
+}
